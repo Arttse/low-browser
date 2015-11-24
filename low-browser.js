703300bf37c2b25539ffea104c1f172810a2fe4e
Nikita Bystrov <arttsesoft@gmail.com>
2015-11-24 16:30:39 +0300

    Added Init Function, EDGE coreVersion

--- low-browser.js
+++ low-browser.js
@@ -3,7 +3,7 @@
  * https://github.com/Arttse/low-browser
  * Copyright (c) 2015 Nikita «Arttse» Bystrov
  * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
- * Version: 0.1.1-dev
+ * Version: 0.1.1
  */
 
 (function ( window ) {
@@ -15,51 +15,68 @@
     /** Create object inside window */
     window.lowBrowser = {};
 
-    /** Check Windows OS */
-    if ( m = userAgent.match( /(Windows.*?)(;|\))/i ) ) {
-        window.lowBrowser.os = m[1];
-    }
+    /** Initialization function */
+    window.lowBrowser.init = function ( userAgent ) {
 
-    /** Check Trident version core */
-    if ( m = userAgent.match( /Trident\/([0-9]+.[0-9]+)(;|\))/i ) ) {
-        window.lowBrowser.core        = 'Trident';
-        window.lowBrowser.coreVersion = m[1];
-    }
+        window.lowBrowser.name         = undefined;
+        window.lowBrowser.version      = undefined;
+        window.lowBrowser.core         = undefined;
+        window.lowBrowser.coreVersion  = undefined;
+        window.lowBrowser.os           = undefined;
+        window.lowBrowser.osBuild      = undefined;
+        window.lowBrowser.gamePlatform = undefined;
 
-    /** Check IE 11 */
-    if ( /rv:11\.0/i.test( userAgent ) && window.lowBrowser.core === 'Trident' ) {
-        window.lowBrowser.name    = 'IE';
-        window.lowBrowser.version = '11.0';
-    }
+        /** Check Windows OS */
+        if ( m = userAgent.match( /(Windows.*?)(;|\))/i ) ) {
+            window.lowBrowser.os = m[1];
+        }
 
-    /** Check IE < 11 */
-    if ( m = userAgent.match( /MSIE ([0-9]+.[0-9]+)(;|\))/i ) ) {
-        window.lowBrowser.name    = 'IE';
-        window.lowBrowser.version = m[1];
-    }
+        /** Check Trident version core */
+        if ( m = userAgent.match( /Trident\/(\d+\.\d+)(;|\))/i ) ) {
+            window.lowBrowser.core        = 'Trident';
+            window.lowBrowser.coreVersion = m[1];
+        }
 
-    /** Check IEMobile */
-    if ( m = userAgent.match( /IEMobile\/([0-9]+.[0-9]+)(;|\))/i ) ) {
-        window.lowBrowser.name    = 'IEMobile';
-        window.lowBrowser.version = m[1];
-    }
+        /** Check IE 11 */
+        if ( /rv:11\.0/i.test( userAgent ) && window.lowBrowser.core === 'Trident' ) {
+            window.lowBrowser.name    = 'IE';
+            window.lowBrowser.version = '11.0';
+        }
 
-    /** Check EDGE browser */
-    if ( m = userAgent.match( /Edge\/(.*?)\.([0-9]+)/i ) ) {
-        window.lowBrowser.name    = 'Edge';
-        window.lowBrowser.version = m[1];
-        window.lowBrowser.osBuild = m[2];
-        window.lowBrowser.core    = 'EdgeHTML';
-    }
+        /** Check IE < 11 */
+        if ( m = userAgent.match( /MSIE (\d+\.\d+)(;|\))/i ) ) {
+            window.lowBrowser.name    = 'IE';
+            window.lowBrowser.version = m[1];
+        }
 
-    /** Check Xbox 360 */
-    if ( /Xbox/i.test( userAgent ) ) {
-        window.lowBrowser.gamePlatform = 'Xbox 360';
-    }
+        /** Check IEMobile */
+        if ( m = userAgent.match( /IEMobile\/(\d+\.\d+)(;|\))/i ) ) {
+            window.lowBrowser.name    = 'IEMobile';
+            window.lowBrowser.version = m[1];
+        }
 
-    /** Check Xbox One */
-    if ( /Xbox One/i.test( userAgent ) ) {
-        window.lowBrowser.gamePlatform = 'Xbox One';
-    }
+        /** Check EDGE browser */
+        if ( m = userAgent.match( /Edge\/(.*?)\.(\d+)/i ) ) {
+            window.lowBrowser.name        = 'Edge';
+            window.lowBrowser.version     = m[1];
+            window.lowBrowser.osBuild     = m[2];
+            window.lowBrowser.core        = 'EdgeHTML';
+            window.lowBrowser.coreVersion = m[1] + '.' + m[2];
+        }
+
+        /** Check Xbox 360 */
+        if ( /Xbox/i.test( userAgent ) ) {
+            window.lowBrowser.gamePlatform = 'Xbox 360';
+        }
+
+        /** Check Xbox One */
+        if ( /Xbox One/i.test( userAgent ) ) {
+            window.lowBrowser.gamePlatform = 'Xbox One';
+        }
+
+    };
+
+    /** Initialization for current browser */
+    window.lowBrowser.init( userAgent );
 
 }) ( window );
